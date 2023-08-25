849a178367da1a2060c349f739ad45e03575d12b
raamNnoodlez <ashirbutt998@gmail.com>
2023-08-25 16:06:47 +0200

    Added door

---
+++ Assets/Scripts/Door_Open.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Open : MonoBehaviour
+{
+    public GameObject[] linkedTorches;
+    public float openSpeed = 10f;
+    public float doorLength = 18f;
+    
+    private Vector3 initialPosition;
+    private Vector3 openPosition;
+    private float targetY;
+    void Start()
+    {
+        initialPosition = transform.position;
+        openPosition = new Vector3(initialPosition.x, initialPosition.y - doorLength, initialPosition.z);
+        targetY = initialPosition.y;
+    }
+    
+    void Update()
+    {
+        calculateTargetPosition();
+        transform.position = Vector3.MoveTowards(transform.position,
+            new Vector3(initialPosition.x, targetY, initialPosition.z), openSpeed * Time.deltaTime);
+    }
+
+    private void calculateTargetPosition()
+    {
+        int litCount = 0;
+        for (int i = 0; i < linkedTorches.Length; i++)
+        {
+            if (linkedTorches[i].transform.childCount > 0 && linkedTorches[i].transform.GetChild(0).gameObject.activeSelf)
+            {
+                litCount++;
+            }
+        }
+
+        float litPercentage = (float)litCount / linkedTorches.Length;
+        targetY = Mathf.Lerp(initialPosition.y, openPosition.y, litPercentage);
+    }
+    
+}
