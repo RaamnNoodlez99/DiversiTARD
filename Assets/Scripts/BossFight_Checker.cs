aabb8be40d47d1e3fcd52b0a66ae75fb1806b498
DianLahouter <lahouterd@gmail.com>
2023-10-02 01:38:11 +0200

    Boss stuff

---
+++ Assets/Scripts/BossFight_Checker.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFight_Checker : MonoBehaviour
+{
+    public GameObject levelOver;
+    public GameObject boss;
+
+    private bool hasCalledLevelOver;
+
+    private void Update()
+    {
+        if(!hasCalledLevelOver && boss == null)
+        {
+            hasCalledLevelOver = true;
+            Invoke("callLevelOver", 2f);
+        }
+    }
+
+    private void callLevelOver()
+    {
+        levelOver.GetComponent<Level_Complete>().LevelOver();
+    }
+}
--- Assets/Scripts/Boss_Phase3.cs
+++ Assets/Scripts/Boss_Phase3.cs
@@ -13,9 +13,12 @@ public class Boss_Phase3 : MonoBehaviour
     private bool isOnCooldown = false;
     private string direction = "left";
     public AudioSource bossRoar;
+    public int bossDamage;
+    public AudioSource bossHurt;
 
     public GameObject bossObject;
     private Animator bossObjectAnimator;
+    private bool isFlickering;
 
     private bool facingLeft = false;
 
@@ -136,11 +139,93 @@ public class Boss_Phase3 : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        hitSomething = true;
+        Player_Controller player = null;
+        if (collision.gameObject.CompareTag("WoodenMan"))
+        {
+            player = GameObject.FindGameObjectWithTag("WoodenMan").GetComponent<Player_Controller>();
+        }
+
+        if (player != null)
+        {
+            player.knockbackCounter = player.knockbackTotalTime;
+
+            if (collision.transform.position.x <= transform.position.x)
+            {
+                player.knockFromRight = true;
+            }
+            else
+            {
+                player.knockFromRight = false;
+            }
+
+            Health objectHealth = collision.collider.GetComponent<Health>();
+            objectHealth.Damage(bossDamage);
+        }
+
+        if (collision.gameObject.CompareTag("Projectile"))
+        {
+            bossHurt.Play();
+        }
+        else
+        {
+            hitSomething = true;
+        }
     }
 
     public void flicker()
     {
 
+    }
+
+    private void DisableRenderers(Transform parentTransform)
+    {
+        foreach (Transform child in parentTransform)
+        {
+            Renderer renderer = child.GetComponent<Renderer>();
+
+            if (renderer != null)
+            {
+                renderer.enabled = false;
+            }
+
+            DisableRenderers(child);
+        }
+    }
+
+    private void EnableRenderers(Transform parentTransform)
+    {
+        foreach (Transform child in parentTransform)
+        {
+            Renderer renderer = child.GetComponent<Renderer>();
+
+            if (renderer != null)
+            {
+                renderer.enabled = true;
+            }
+
+            EnableRenderers(child);
+        }
+    }
+
+    IEnumerator Flicker(Transform parentTransform)
+    {
+        isFlickering = true;
+
+        for (int i = 0; i < 5; i++)
+        {
+            EnableRenderers(parentTransform);
+            yield return new WaitForSeconds(0.1f);
+            DisableRenderers(parentTransform);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        EnableRenderers(parentTransform);
+        isFlickering = false;
+    }
+
+    public void flickerBoss()
+    {
+        if (!isFlickering)
+            StartCoroutine(Flicker(gameObject.transform));
     }
 }
